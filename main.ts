2dedf1d70d4f3bf0e99d9a28ca5e25f6ae186845
jBell01 <james.bell@student.csd509j.net>
2025-06-02 11:41:34 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,44 @@
+/**
+ * Constant variables
+ */
+/**
+ * Classes
+ */
+/**
+ * Global variables
+ */
+// Event handlers
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    showSprite(sprites.create(list._pickRandom(), SpriteKind.Player))
+})
+// Functions
+function showSprite (mySprite: Sprite) {
+    mySprite.setPosition(randint(0, scene.screenWidth() - 8), randint(0, scene.screenHeight() - 8))
+}
+let list: Image[] = []
+tiles.setCurrentTilemap(tilemap`level2`)
+let player1 = sprites.create(img`
+    . . . . . . f f f f . . . . . . 
+    . . . . f f f 2 2 f f f . . . . 
+    . . . f f f 2 2 2 2 f f f . . . 
+    . . f f f e e e e e e f f f . . 
+    . . f f e 2 2 2 2 2 2 e e f . . 
+    . . f e 2 f f f f f f 2 e f . . 
+    . . f f f f e e e e f f f f . . 
+    . f f e f b f 4 4 f b f e f f . 
+    . f e e 4 1 f d d f 1 4 e e f . 
+    . . f e e d d d d d d e e f . . 
+    . . . f e e 4 4 4 4 e e f . . . 
+    . . e 4 f 2 2 2 2 2 2 f 4 e . . 
+    . . 4 d f 2 2 2 2 2 2 f d 4 . . 
+    . . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
+    . . . . . f f f f f f . . . . . 
+    . . . . . f f . . f f . . . . . 
+    `, SpriteKind.Player)
+// Main program
+list = [
+assets.image`myCat`,
+assets.image`mySnake`,
+assets.image`myShroom`,
+assets.image`myDonut`
+]

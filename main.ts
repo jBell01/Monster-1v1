14dbf406cabaab47902a19dd2add069f66b13814
jBell01 <james.bell@student.csd509j.net>
2025-06-02 11:44:46 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -7,15 +7,6 @@
 /**
  * Global variables
  */
-// Event handlers
-controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
-    showSprite(sprites.create(list._pickRandom(), SpriteKind.Player))
-})
-// Functions
-function showSprite (mySprite: Sprite) {
-    mySprite.setPosition(randint(0, scene.screenWidth() - 8), randint(0, scene.screenHeight() - 8))
-}
-let list: Image[] = []
 tiles.setCurrentTilemap(tilemap`level2`)
 let player1 = sprites.create(img`
     . . . . . . f f f f . . . . . . 
@@ -35,10 +26,3 @@ let player1 = sprites.create(img`
     . . . . . f f f f f f . . . . . 
     . . . . . f f . . f f . . . . . 
     `, SpriteKind.Player)
-// Main program
-list = [
-assets.image`myCat`,
-assets.image`mySnake`,
-assets.image`myShroom`,
-assets.image`myDonut`
-]

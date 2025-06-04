2ae2377b123fab711d8b296e28d25a6a2ff0d574
jBell01 <james.bell@student.csd509j.net>
2025-06-03 19:04:28 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -126,73 +126,73 @@ function batSpawn () {
     characterAnimations.loopFrames(
     bat,
     [img`
-        f f f . . . . . . . . f f f . . 
-        c b b c f . . . . . . c c f f . 
-        . c b b c f . . . . . . c c f f 
-        . c c c b f . . . . . . c f c f 
-        . c c b b c f . c c . c c f f f 
-        . c b b c b f c c 3 c c 3 c f f 
-        . c b c c b f c b 3 c b 3 b f f 
-        . . c c c b b c b b b b b b c . 
-        . . . c c c c b b 1 b b b 1 c . 
-        . . . . c c b b b b b b b b b c 
-        . . . . f b b b b c b b b c b c 
-        . . . c f b b b b 1 f f f 1 b f 
-        . . c c f b b b b b b b b b b f 
-        . . . . f c b b b b b b b b f . 
-        . . . . . f c b b b b b b f . . 
-        . . . . . . f f f f f f f . . . 
+        . . f f f . . . . . . . . f f f 
+        . f f c c . . . . . . f c b b c 
+        f f c c . . . . . . f c b b c . 
+        f c f c . . . . . . f b c c c . 
+        f f f c c . c c . f c b b c c . 
+        f f c 3 c c 3 c c f b c b b c . 
+        f f b 3 b c 3 b c f b c c b c . 
+        . c b b b b b b c b b c c c . . 
+        . c 1 b b b 1 b b c c c c . . . 
+        c b b b b b b b b b c c . . . . 
+        c b c b b b c b b b b f . . . . 
+        f b 1 f f f 1 b b b b f c . . . 
+        f b b b b b b b b b b f c c . . 
+        . f b b b b b b b b c f . . . . 
+        . . f b b b b b b c f . . . . . 
+        . . . f f f f f f f . . . . . . 
         `,img`
-        . . . . . . . . . . . f f f . . 
-        f f f . . . . . . . . c c f f f 
-        c b b c f . . . c c . . c c f f 
-        . c b b b f f c c 3 c c 3 c f f 
-        . c c c b b f c b 3 c b 3 b f f 
-        . c c b c b f c b b b b b b c . 
-        . c b b c b b c b b b b b b c . 
-        . c b c c c b b b 1 b b b 1 b c 
-        . . c c c c c b b b b b b b b c 
-        . . . c f b b b b c b b b c b f 
-        . . c c f b b b b 1 f f f 1 b f 
-        . . . . f c b b b b b b b b f . 
-        . . . . . f c b b b b b b f . . 
-        . . . . . . f f f f f f f . . . 
+        . . f f f . . . . . . . . . . . 
+        f f f c c . . . . . . . . f f f 
+        f f c c . . c c . . . f c b b c 
+        f f c 3 c c 3 c c f f b b b c . 
+        f f b 3 b c 3 b c f b b c c c . 
+        . c b b b b b b c f b c b c c . 
+        . c b b b b b b c b b c b b c . 
+        c b 1 b b b 1 b b b c c c b c . 
+        c b b b b b b b b c c c c c . . 
+        f b c b b b c b b b b f c . . . 
+        f b 1 f f f 1 b b b b f c c . . 
+        . f b b b b b b b b c f . . . . 
+        . . f b b b b b b c f . . . . . 
+        . . . f f f f f f f . . . . . . 
         . . . . . . . . . . . . . . . . 
         . . . . . . . . . . . . . . . . 
         `,img`
         . . . . . . . . . . . . . . . . 
-        . . . . . . . . c c . . c c . . 
-        . . . . . . c c c 3 c c 3 c . . 
-        . . . . . c c c b 3 c b 3 b c . 
-        . . . . f f b b b b b b b b c . 
-        . . . . f f b b b b b b b b c c 
-        . . . f f f c b b 1 b b b 1 b c 
-        . . . f f f f b b b b b b b b c 
-        . . . b b b c c b c b b b c b f 
-        . . . c c c c f b 1 f f f 1 b f 
-        . . . c c b b f b b b b b b f . 
-        . . . c b b c c b b b b b f c c 
-        . . c b b c c f f f f f f c c c 
-        . c c c c c . . . . . . c c c . 
-        c c c c . . . . . . . c c c . . 
+        . . c c . . c c . . . . . . . . 
+        . . c 3 c c 3 c c c . . . . . . 
+        . c b 3 b c 3 b c c c . . . . . 
+        . c b b b b b b b b f f . . . . 
+        c c b b b b b b b b f f . . . . 
+        c b 1 b b b 1 b b c f f f . . . 
+        c b b b b b b b b f f f f . . . 
+        f b c b b b c b c c b b b . . . 
+        f b 1 f f f 1 b f c c c c . . . 
+        . f b b b b b b f b b c c . . . 
+        c c f b b b b b c c b b c . . . 
+        c c c f f f f f f c c b b c . . 
+        . c c c . . . . . . c c c c c . 
+        . . c c c . . . . . . . c c c c 
         . . . . . . . . . . . . . . . . 
         `,img`
         . f f f . . . . . . . . f f f . 
-        . c b b c f . . . . . . . c f f 
-        . . c b b c f . . . . . . c c f 
-        . . c c c b f . . . . . . . f c 
-        . . c c b b f f . . . . . f f c 
-        . . c b b c b f c c . c c f f f 
-        . . c b c c b f c c c c c f f f 
-        . . . c c c b c b 3 c c 3 c f . 
-        . . . c c c c b b 3 c b 3 b c . 
-        . . . . c c b b b b b b b b c c 
-        . . . c f b b b b 1 b b b 1 b c 
-        . . c c f b b b b b b b b b b f 
-        . . . . f b b b b c b b b c b f 
-        . . . . f c b b b 1 f f f 1 f . 
-        . . . . . f c b b b b b b f . . 
-        . . . . . . f f f f f f f . . . 
+        f f c . . . . . . . f c b b c . 
+        f c c . . . . . . f c b b c . . 
+        c f . . . . . . . f b c c c . . 
+        c f f . . . . . f f b b c c . . 
+        f f f c c . c c f b c b b c . . 
+        f f f c c c c c f b c c b c . . 
+        . f c 3 c c 3 b c b c c c . . . 
+        . c b 3 b c 3 b b c c c c . . . 
+        c c b b b b b b b b c c . . . . 
+        c b 1 b b b 1 b b b b f c . . . 
+        f b b b b b b b b b b f c c . . 
+        f b c b b b c b b b b f . . . . 
+        . f 1 f f f 1 b b b c f . . . . 
+        . . f b b b b b b c f . . . . . 
+        . . . f f f f f f f . . . . . . 
         `],
     100,
     characterAnimations.rule(Predicate.NotMoving)
@@ -332,9 +332,6 @@ function skeletonSpawn () {
     100,
     characterAnimations.rule(Predicate.NotMoving)
     )
-}
-function fightEnemy () {
-	
 }
 let fightingSnake = false
 let fightingSkeleton = false
